b68c68d7becc93389d3ae1d1da2c93e35e75401d
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-09-22 17:38:10 +0200

    More game logic

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,5 @@
+
+
 #[cfg(test)]
 ///The game logic.
 ///The game does not know which players are human,
@@ -50,6 +52,15 @@ impl Game {
     }
 
     #[cfg(test)]
+    pub fn do_move(&mut self, x: usize, y: usize) -> () {
+        assert!(self.can_do_move(x, y));
+        self.set_square(x, y, self.get_active_player());
+        self.m_player = ((self.m_player) + 1) % crate::band::get_band_size() as i8;
+        assert!(self.m_player >= 0);
+        assert!(self.m_player < crate::band::get_band_size() as i8);
+    }
+
+    #[cfg(test)]
     pub fn get_active_player(&self) -> i8 {
         self.m_player
     }
@@ -65,27 +76,63 @@ impl Game {
     }
 
     #[cfg(test)]
+    /// Get the value of a square
+    /// @return value of the square:
+    /// - -1: empty
+    /// - 0: player 1
+    /// - 1: player 2
+    /// - 2: player 3
+    pub fn get_square(&self, x: usize, y: usize) -> i8 {
+        assert!(self.can_get_square(x, y));
+        self.m_area[y][x]
+    }
+
+    /// Is the game a draw?
+    #[cfg(test)]
+    pub fn is_draw(&self) -> bool {
+        for x in 0..self.get_n_cols() {
+            for y in 0..self.get_n_rows() {
+              if self.is_empty(x, y) {
+                  return false;
+              }
+            }
+        }
+        true
+    }
+
+    #[cfg(test)]
     pub fn is_empty(&self, x: usize, y: usize) -> bool {
         assert!(self.can_get_square(x,y));
-        self.m_area[y][x] == -1
+        self.get_square(x, y) == -1
+    }
+
+    /// Restart the game, maintaining the arena's size
+    #[cfg(test)]
+    pub fn restart(&mut self) -> () {
+        self.m_area = vec![vec![-1; self.get_n_cols()]; self.get_n_rows()];
+        self.m_player = 0;
+    }
+
+    #[cfg(test)]
+    /// Set the value of a square
+    /// @param square_value value of the square:
+    /// - -1: empty
+    /// - 0: player 1
+    /// - 1: player 2
+    /// - 2: player 3
+    pub fn set_square(&mut self, x: usize, y: usize, square_value: i8) -> () {
+        assert!(self.can_get_square(x, y));
+        self.m_area[y][x] = square_value;
+        assert_eq!(self.get_square(x, y), square_value)
     }
 }
 
 /*
 struct Game
 {
-  explicit Game(
-    const int n_cols = 16,
-    const int n_rows = 12
-  );
-
-  bool can_do_move(const int x, const int y) const noexcept;
-  int CanGetSquare(const int x, const int y) const noexcept;
   void DoMove(const int x, const int y);
   void DoMove(const Move& p) noexcept;
   Square GetSquare(const int x, const int y) const noexcept;
-  static std::string GetVersion() noexcept;
-  static std::vector<std::string> GetVersionHistory() noexcept;
 
   ///GetWinner returns the index of the winner.
   ///Returns draw if the board is full without a winner
@@ -100,7 +147,6 @@ struct Game
 
 private:
 
-  bool can_do_move(const Move& p) const noexcept;
   Move CheckOneOther(const std::bitset<3>& is_player_human) const;
   Move CheckTwoDiagonally() const;
   Move CheckTwoHorizontalOwn() const;
@@ -109,7 +155,6 @@ private:
 
   ///Returns all possible moves
   std::vector<Move> GetAllPossibleMoves() const noexcept;
-  Player GetNextPlayer() const noexcept;
   std::vector<Move> GetTwoHorizontalOtherMoves() const noexcept;
   std::vector<Move> GetTwoVerticalOtherMoves() const noexcept;
 
@@ -165,6 +210,35 @@ mod tests {
         assert!(game.is_empty(0, 8));
     }
 
+    #[test]
+    fn test_set_square() {
+        let x = 3;
+        let y = 2;
+        let player_2 = 1; // Square value
+        let empty = -1; // Square value
+        assert!(x != y);
+        let mut game = Game::default();
+        assert!(game.is_empty(x, y));
+        game.set_square(x, y, player_2);
+        assert!(!game.is_empty(x, y));
+        game.set_square(x, y, empty);
+        assert!(game.is_empty(x, y));
+    }
+    #[test]
+    fn test_get_square() {
+        let x = 3;
+        let y = 2;
+        let player_2 = 1; // Square value
+        let empty = -1; // Square value
+        assert!(x != y);
+        let mut game = Game::default();
+        assert!(game.get_square(x, y) != player_2);
+        game.set_square(x, y, player_2);
+        assert!(game.get_square(x, y) == player_2);
+        game.set_square(x, y, empty);
+        assert!(game.get_square(x, y) != player_2);
+    }
+
 
     #[test]
     fn test_can_do_move() {
@@ -172,6 +246,60 @@ mod tests {
         assert!(game.can_do_move(0, 0));
         assert!(!game.can_do_move(100, 0));
         assert!(!game.can_do_move(0, 100));
+    }
+
+    #[test]
+    fn test_do_move_once() {
+        let x = 7;
+        let y = 3;
+        let mut game = Game::default();
+        assert_eq!(game.get_active_player(), 0);
+        assert!(game.is_empty(x, y));
+        game.do_move(x, y);
+        assert_eq!(game.get_active_player(), 1);
+        assert!(!game.is_empty(x, y));
+        assert_eq!(game.get_square(x, y), 0); // Player 1
+    }
+
+    #[test]
+    fn test_do_move_changes_player() {
+        let mut game = Game::default();
+        assert_eq!(game.get_active_player(), 0);
+        game.do_move(0, 0);
+        assert_eq!(game.get_active_player(), 1);
+        game.do_move(1, 0);
+        assert_eq!(game.get_active_player(), 2);
+        game.do_move(2, 0);
+        assert_eq!(game.get_active_player(), 0);
+    }
+
+    #[test]
+    fn test_is_draw() {
+        let mut game = Game::new(2, 2);
+        assert!(!game.is_draw());
+        game.do_move(0, 0);
+        assert!(!game.is_draw());
+        game.do_move(0, 1);
+        assert!(!game.is_draw());
+        game.do_move(1, 0);
+        assert!(!game.is_draw());
+        game.do_move(1, 1);
+        assert!(game.is_draw());
+    }
+
+    #[test]
+    fn test_restart() {
+        let mut game = Game::new(2, 2);
+        assert!(!game.is_draw());
+        game.do_move(0, 0); // done by player 1
+        game.do_move(0, 1); // done by player 2
+        game.do_move(1, 0); // done by player 3
+        game.do_move(1, 1); // done by player 1
+        assert!(game.is_draw());
+        assert_eq!(game.get_active_player(), 1); // 1 == player 2
+        game.restart();
+        assert!(!game.is_draw());
+        assert_eq!(game.get_active_player(), 0); // 0 == player 1
     }
 
     #[test]
